e51ef8b5aae3e92c477aa86bc3ed3663e31fe69e
zeviberlin <zeviberlin@gmail.com>
2025-12-22 11:55:01 -0500

    String done

--- String.h
+++ String.h
@@ -22,50 +22,82 @@ typedef struct {
 
 
 /// GETTERS ///
-char* getStringData(String* str) {
+static const char* getStringData(String* str) {
 	return str->__data;
 }
 
-size_t getStringSize(String* str) {
+static size_t getStringSize(String* str) {
 	return str->__size;
 }
 
-size_t getStringEnd(String* str) {
+static size_t  getStringEnd(String* str) {
 	return str->__end;
 }
 ///
 
 
 /// MODIFIERS ///
-void resizeString(String* str, size_t new_size) {
+static void resizeString(String* str, size_t new_size) {
 	str->__size = new_size;
 	str->__data = realloc(str->__data, str->__size);
+}
+
+static void charAppendString(String* str, char* app) {
+	size_t app_len = len(app);
+	
+	if (str->__size < str->__end + app_len) {
+		resizeString(str, str->__end + app_len);
+	}
+
+	for (size_t i = 0; i < app_len; i++) {
+		str->__data[str->__end] = app[i];
+		str->__end++;
+	}
+}
+
+static void stringAppendString(String* str, String* app) {
+	charAppendString(str, app->__data);
+}
+
+static String* createSubstring(String* str, size_t begindex, size_t endex) {
+	String* new_str = (String*)(malloc(sizeof(String)));
+	new_str->__end = new_str->__size;
+
+	resizeString(new_str, endex - begindex);
+
+	size_t idx = 0;
+	for (size_t i = begindex; i < endex; i++) {
+		new_str->__data[idx] = str->__data[i];
+		idx++;
+	}
+
+	return new_str;
 }
 /// ///
 
 
 /// CONSTRUCTORS & DESTRUCTORS ///
-String* createString(char* initial) {
+static String* createString(char* initial) {
 	String* new_str = (String*)(malloc(sizeof(String)));
 	new_str->__size = len(initial);
 	new_str->__end = len(initial);
 
 	resizeString(new_str, new_str->__size);
 
-	for (int i = 0; i < new_str->__size; i++) {
+	for (size_t i = 0; i < new_str->__size; i++) {
 		new_str->__data[i] = initial[i];
 	}
 
 	return new_str;
 }
 
-String* copyString(String* str) {
+static String* copyString(String* str) {
 	return createString(str->__data);
 }
 
 //
 
-void destroyString(String* str) {
+static void destroyString(String* str) {
 	if (str->__size > 0) free(str->__data);
 	str->__size = 0;
 	str->__end = 0;
--- test.c
+++ test.c
@@ -6,10 +6,32 @@
 
 int main() {
 	/// STRING TESTING ///
+	printf("┏━━━━━━━━━━━━━━━━━━━━━━STRING TESTING━━━━━━━━━━━━━━━━━━━━━━┓\n");
+	
 	String* str = createString("Hello");
-	printf("str: %s, size: %lu\n", getStringData(str), getStringSize(str));
+	printf("┃ CREATE                     str: %s | size: %lu          ┃\n", getStringData(str), getStringSize(str));
+
+	resizeString(str, 11);
+	printf("┃ RESIZE (11)                str: %s | size: %lu         ┃\n", getStringData(str), getStringSize(str));
+
+	String* toApp = createString(", worl");
+	stringAppendString(str, toApp);
+	destroyString(toApp);
+	printf("┃ APPEND STRING (\", world\")  str: %s | size: %lu   ┃\n", getStringData(str), getStringSize(str));
+
+	charAppendString(str, "d!");
+	printf("┃ APPEND CHAR* (\"d!\")        str: %s | size: %lu ┃\n", getStringData(str), getStringSize(str));
+
+	String* substr = createSubstring(str, 7, 12);
+	printf("┃ SUBSTRING (7 - 12)         substr: %s                 ┃\n", getStringData(substr));
+	destroyString(substr);
+
+	resizeString(str, 4);
+	printf("┃ RESIZE (4)                 str: %s | size: %lu           ┃\n", getStringData(str), getStringSize(str));
 
 	destroyString(str);
+	
+	printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
 	/// ///
 
 	return 0;
